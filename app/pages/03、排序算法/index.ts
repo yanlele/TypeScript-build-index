62853de6909ce56550a29c5bf8554ce5fee6682e
yanlele <331393627@qq.com>
2018-11-26 11:27:42 +0800

    [+]所有的排序算法都整理了一遍

--- app/pages/03、排序算法/index.ts
+++ app/pages/03、排序算法/index.ts
@@ -4,6 +4,7 @@
  */
 
 let arrNumber: number[] = [12, 22, 34, 56, 11, 3, 77, 39, 32];
+
 class Index {
     main() {
 
@@ -25,9 +26,58 @@ class Index {
         }
         return this.quickSort(left).concat([middle], this.quickSort(right))
     }
+
+    dubbleSort(arr: number[]) {
+        let len: number = arr.length;
+        for (let i: number = 0; i < len - 1; i++) {
+            for (let j: number = i - 1; j < len - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    let temp: number = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    selectionSort(arr: number[]) {
+        let len: number = arr.length;
+        let minIndex: number, temp: number;
+        for (let i: number = 0; i < len - 1; i++) {
+            minIndex = 1;
+            for (let j: number = i + 1; j < len; j++) {
+                if (arr[minIndex] > arr[j]) {
+                    minIndex = j;
+                }
+            }
+
+            temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        return arr;
+    }
+
+    insertSort(arr: number[]) {
+        let len: number = arr.length;
+        let preindex: number, current: number;
+        for (let i: number = 1; i < len; i++) {
+            preindex = i -1;
+            current = arr[i];
+            while (preindex >=0 && arr[preindex] > current) {
+                arr[preindex+ 1] = arr[preindex];
+                preindex --;
+            }
+            arr[preindex + 1] = current;
+        }
+        return arr;
+    }
 }
 
 let index = new Index();
-console.log(index.quickSort(arrNumber));
 
-document.write(index.quickSort(arrNumber));
+console.log(index.quickSort(arrNumber));
+console.log(index.dubbleSort(arrNumber));
+console.log(index.selectionSort(arrNumber));
+console.log(index.insertSort(arrNumber));
