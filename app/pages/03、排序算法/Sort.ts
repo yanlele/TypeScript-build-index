870d20311aaf8393956e777c97de7b994206046d
yanlele <331393627@qq.com>
2018-11-28 00:03:28 +0800

    [+]所有JS 算法都实现了一遍

--- app/pages/03、排序算法/Sort.ts
+++ app/pages/03、排序算法/Sort.ts
@@ -55,16 +55,15 @@ class Sort {
 
     insertSort(arr: number[]) {
         let len = arr.length;
-        let preIndex: number, current: number;
-
-        for (let i : number = 1; i < len;i++) {
+        let preIndex: number , current: number;
+        for (let i : number = 1; i< len;i++) {
             preIndex = i -1;
             current = arr[i];
-            while (preIndex >= 0 && arr[preIndex] > current) {
+            while (preIndex >=0 && arr[preIndex] > current) {
                 arr[preIndex + 1] = arr[preIndex];
                 preIndex --;
             }
-            arr[preIndex+ 1] =current;
+            arr[preIndex + 1] = current
         }
         return arr;
     }
