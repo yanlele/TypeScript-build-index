c9917e2bc01190c64fbf976a58e00d5dedc0cf1e
yanlele <331393627@qq.com>
2018-11-28 17:29:25 +0800

    [+]从新实现一遍排序算法

--- app/pages/03、排序算法/Sort.ts
+++ app/pages/03、排序算法/Sort.ts
@@ -59,18 +59,15 @@ class Sort {
     }
 
     insertSort(arr: number[]) {
-        let len: number = arr.length;
-        let preIndex: number, current: number;
-        for (let i: number = 1; i < len; i++) {
-            preIndex = i - 1;
+        let preIndex: number, current: number, len: number = arr.length;
+        for (let i : number = 1;i< len;i++) {
+            preIndex = i -1;
             current = arr[i];
-            while (preIndex >= 0 && arr[preIndex] > current) {
-                arr[preIndex+1] = arr[preIndex];
+            while (preIndex >=0 && arr[preIndex] > current) {
+                arr[preIndex + 1] = arr[preIndex];
                 preIndex --;
             }
-            arr[preIndex + 1] = current;
         }
-        return arr
     }
 }
 
