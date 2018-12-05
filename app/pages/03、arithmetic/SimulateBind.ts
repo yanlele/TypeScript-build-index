8b848b4903cf504eaed67af52c0e8f44cdc7b4a6
yanlele <331393627@qq.com>
2018-12-05 15:48:22 +0800

    [+]想是实现一个bind

---
+++ app/pages/03、arithmetic/SimulateBind.ts
@@ -0,0 +1,18 @@
+
+
+let yanle = {
+    name: 'yanle',
+    sayHello: function (age) {
+        console.log(`hello, i am ${this.name} and ${age} years old`);
+    }
+};
+let lele = {
+    name: 'lele'
+};
+
+class SimulateBind {
+    
+}
+
+
+export default SimulateBind;
