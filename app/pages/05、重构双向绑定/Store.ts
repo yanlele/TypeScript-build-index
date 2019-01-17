521cb14ec3e538c847c5ecd77c0dc48548a547e8
yanlele <331393627@qq.com>
2019-01-17 23:52:21 +0800

    [+]添加上想绑定

---
+++ app/pages/05、重构双向绑定/Store.ts
@@ -0,0 +1,11 @@
+/**
+ * create by yanle
+ * create time 2019/1/17 11:51 PM
+ */
+
+/**
+ * 单例模式的存储对象
+ */
+class Store {
+
+}
---
+++ app/pages/05、重构双向绑定/index.ts
@@ -0,0 +1,12 @@
+/**
+ * create by yanle
+ * create time 2019/1/17 11:48 PM
+ */
+class Index {
+    run() {
+        console.log('run');
+    }
+}
+
+let index = new Index();
+index.run();
