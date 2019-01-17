e56791ef09515ce341ec611bfa98b11e0a0b72fd
yanlele <331393627@qq.com>
2019-01-17 23:09:47 +0800

    [+]整理快排

--- app/pages/03、arithmetic/Sort.ts
+++ app/pages/03、arithmetic/Sort.ts
@@ -28,14 +28,14 @@ class Sort {
         if (arr.length < 1) {
             return arr;
         }
-        let middle: number,
-            middleIndex: number,
-            left: number[] = [],
-            right: number[] = [];
-        middleIndex = Math.floor(arr.length / 2);
+        let middleIndex: number;
+        let middle: number;
+        let left: number[] = [];
+        let right: number[] = [];
+        middleIndex = Math.floor(arr.length/2);
         middle = arr.splice(middleIndex, 1)[0];
-        for (let i: number = 0; i < arr.length; i++) {
-            if (arr[i] < middle) {
+        for (let i : number = 0;i< arr.length;i++) {
+            if(arr[i] < middle) {
                 left.push(arr[i])
             } else {
                 right.push(arr[i])
@@ -62,9 +62,9 @@ class Sort {
             len: number = arr.length;
         for (let i: number = 0; i < arr.length - 1; i++) {
             for (let j: number = 0; j < arr.length - 1 - i; j++) {
-                if(arr[j]> arr[j+1]) {
-                    temp = arr[j+1];
-                    arr[j+1] = arr[j];
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
                     arr[j] = temp;
                 }
             }
@@ -94,10 +94,10 @@ class Sort {
         let minIndex: number,
             temp: number,
             len: number = arr.length;
-        for (let i: number = 0; i< len -1; i ++) {
+        for (let i: number = 0; i < len - 1; i++) {
             minIndex = i;
-            for (let j: number = i+1;j< len;j++) {
-                if(arr[minIndex] > arr[j]) {
+            for (let j: number = i + 1; j < len; j++) {
+                if (arr[minIndex] > arr[j]) {
                     minIndex = j
                 }
             }
@@ -125,12 +125,12 @@ class Sort {
         let len: number = arr.length,
             temp: number,
             current: number;
-        for (let i: number = 1; i< len; i++) {
+        for (let i: number = 1; i < len; i++) {
             current = arr[i];
-            for (let j: number = i -1; j>=0 && arr[j]>current;j--) {
+            for (let j: number = i - 1; j >= 0 && arr[j] > current; j--) {
                 temp = arr[i];
-                arr[i] = arr[j+1];
-                arr[j+1] = temp;
+                arr[i] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
         return arr;
@@ -151,15 +151,15 @@ class Sort {
         }
         return arr;*/
 
-        let len: number  = arr.length,
-            gap: number = Math.floor(len/2),
+        let len: number = arr.length,
+            gap: number = Math.floor(len / 2),
             temp: number;
-        for (gap;gap>0;gap=Math.floor(gap/2)) {
-            for (let i : number = 1; i < len; i++) {
-                for(let j: number = i - gap;j>=0&& arr[j] > arr[j+gap];j-=gap){
+        for (gap; gap > 0; gap = Math.floor(gap / 2)) {
+            for (let i: number = 1; i < len; i++) {
+                for (let j: number = i - gap; j >= 0 && arr[j] > arr[j + gap]; j -= gap) {
                     temp = arr[j];
-                    arr[j] = arr[j+gap];
-                    arr[j+gap] = temp;
+                    arr[j] = arr[j + gap];
+                    arr[j + gap] = temp;
                 }
             }
         }
