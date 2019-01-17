8c2d0243e31a115cec15b34c5c7599677b107e0e
yanlele <331393627@qq.com>
2019-01-18 00:09:17 +0800

    [+]利用单例模式实现一个存储对象

--- app/pages/05、重构双向绑定/Store.ts
+++ app/pages/05、重构双向绑定/Store.ts
@@ -7,7 +7,24 @@
  * 单例模式的存储对象
  */
 class Store {
+    private static instance: Store;
 
+    static get _instance(): Store {
+        return this.instance;
+    }
+
+    static set _instance(value: Store) {
+        this.instance = value;
+    }
+
+    static getInstance() {
+        if(!this._instance) {
+            this._instance = new Store();
+        }
+        return this._instance;
+    }
 }
+
+export default Store;
 
 
--- app/pages/05、重构双向绑定/index.ts
+++ app/pages/05、重构双向绑定/index.ts
@@ -2,9 +2,15 @@
  * create by yanle
  * create time 2019/1/17 11:48 PM
  */
+import Store from "./Store";
+
+
 class Index {
     run() {
         console.log('run');
+
+        console.log(Store.getInstance());
+
     }
 }
 
