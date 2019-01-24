58b932d858ea5be1fff897aa69384e407aa80c20
yanlele <331393627@qq.com>
2019-01-24 14:58:21 +0800

    [+]添加一个简单的jest

--- Test/sum.test.js
+++ Test/sum.test.js
@@ -9,3 +9,21 @@ import {sum} from './sum';
 test('adds 1 + 2 equal 3',  ()=> {
     expect(sum(1,2)).toBe(3)
 });
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
