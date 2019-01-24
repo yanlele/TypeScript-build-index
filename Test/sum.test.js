1e2253350ee75cd7cc51d2cd890ae90a86dd0719
yanlele <331393627@qq.com>
2019-01-24 14:55:36 +0800

    [+]添加一个简单的jest

---
+++ Test/sum.test.js
@@ -0,0 +1,11 @@
+/**
+ * create by yanle
+ * connect me 331393627@qq.com
+ * create time 2019-01-24 14:49
+ */
+
+import {sum} from './sum';
+
+test('adds 1 + 2 equal 3',  ()=> {
+    expect(sum(1,2)).toBe(3)
+});
