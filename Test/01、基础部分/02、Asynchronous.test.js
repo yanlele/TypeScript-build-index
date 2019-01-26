fa1c28e1464648e1af28f23195270a17341b13ad
yanlele <331393627@qq.com>
2019-01-27 00:28:58 +0800

    [+]promise 的测试通过

--- Test/01、基础部分/02、Asynchronous.test.js
+++ Test/01、基础部分/02、Asynchronous.test.js
@@ -41,10 +41,30 @@ describe('Asynchronous', function () {
         // 这种方式并不能非常好的拒绝测试， 因为还是会放过测试用例
         return returnPromise()
             .then(flag => {
-                expect(flag).toBeFalsy();
+                expect(flag).toBeTruthy();
             })
     });
 
+    test('Promise testing reject', function () {
+        let timer = +new Date();
+        let flag = timer % 2 === 1 ? true : false;
+        let returnPromise = function () {
+            return new Promise((resolve, reject) => {
+                if (flag) {
+                    resolve(flag)
+                } else {
+                    reject(flag)
+                }
+            });
+        };
+
+        // 这种方式并不能非常好的拒绝测试， 因为还是会放过测试用例
+        return returnPromise()
+            .catch(flag => {
+                expect.assertions(1);
+                expect(flag).tobeFalsy();
+            })
+    });
 
     test('Promise testing used .resolves and .rejects', function () {
         let timer = +new Date();
