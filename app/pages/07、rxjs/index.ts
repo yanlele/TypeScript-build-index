2ca5866e6533e18f43a6f5b8f7786c6abda68d7b
yanle <yanle@yanledeMacBook-Pro.local>
2019-02-01 22:13:49 +0800

    添加修改

--- app/pages/07、rxjs/index.ts
+++ app/pages/07、rxjs/index.ts
@@ -1,6 +1,7 @@
-import {Observable} from 'rxjs';
+import {interval, Observable} from 'rxjs';
+import {map} from "rxjs/operators";
 
-const source$ = new Observable(observer => {
+/*const source$ = new Observable(observer => {
     let number = 1;
     setInterval(() => {
         observer.next(number++)
@@ -13,4 +14,9 @@ const subscription = source$.subscribe(observer);
 
 setTimeout(() => {
     subscription.unsubscribe()
-}, 5000);
+}, 5000);*/
+
+const source$ = interval(1000).pipe(
+    map(x => x * x)
+);
+source$.subscribe(x => console.log(x));
