75e56e2cb9c1f8a0e64cc8ba453e65b897489cc3
yanle <yanle@yanledeMacBook-Pro.local>
2019-02-28 21:06:27 +0800

    add demo operator

--- app/pages/07、rxjs/index.ts
+++ app/pages/07、rxjs/index.ts
@@ -1,8 +1,12 @@
 import SimpleDemo from './demo2/01、simpleDemo';
+import Operator from './demo2/02、operators';
 
 // SimpleDemo.main();
 // SimpleDemo.observableDemo();
 // SimpleDemo.asyncDemo();
 // SimpleDemo.observeComplete();
 // SimpleDemo.observeError();
-SimpleDemo.observeUnsubscribe();
+// SimpleDemo.observeUnsubscribe();
+
+
+Operator.simple();
