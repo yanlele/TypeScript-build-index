635dd158ca0b4be161382a6cd4bc21037e50830f
yanle <yanle@yanledeMacBook-Pro.local>
2019-03-01 11:12:31 +0800

    add tslint config

--- app/pages/07、rxjs/demo2/03、Observable.ts
+++ app/pages/07、rxjs/demo2/03、Observable.ts
@@ -1,26 +1,26 @@
-import {Observable, of, fromEventPattern} from 'rxjs'
+import {Observable, of, fromEventPattern} from 'rxjs';
 
 class ObservableClass {
-    static simple() {
-        const source$ = new Observable(observe => {
-            observe.next(1);
-            observe.next(1);
-            observe.next(1);
-        });
-        const observer = {
-            next: item => console.log(item),
-        };
+  static simple() {
+    const source$ = new Observable(observe => {
+      observe.next(1);
+      observe.next(1);
+      observe.next(1);
+    });
+    const observer = {
+      next: item => console.log(item),
+    };
 
-        const subscription = source$.subscribe(observer)
-    }
+    const subscription = source$.subscribe(observer);
+  }
 
-    static ofFunc() {
-        const source$ = of(1, 2, 3);
-        const observer = {
-            next: item => console.log(item),
-        };
-        const subscription = source$.subscribe(observer);
-    }
+  static ofFunc() {
+    const source$ = of(1, 2, 3);
+    const observer = {
+      next: item => console.log(item),
+    };
+    const subscription = source$.subscribe(observer);
+  }
 }
 
 export default ObservableClass;
