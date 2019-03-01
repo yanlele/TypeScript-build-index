e9dda077e647700c5155ff0ff682bcdd08ece6cb
yanle <yanle@yanledeMacBook-Pro.local>
2019-03-01 19:39:06 +0800

    modify formate

--- app/pages/07、rxjs/demo2/02、operators.ts
+++ app/pages/07、rxjs/demo2/02、operators.ts
@@ -1,15 +1,14 @@
-import {interval} from "rxjs";
-import {map} from "rxjs/operators";
-
+import {interval} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 class Operator {
-    static simple() {
-        const source$ = interval(1000).pipe(
-            map(x => x * 2)
-        );
+  static simple() {
+    const source$ = interval(1000).pipe(
+        map(x => x * 2),
+    );
 
-        source$.subscribe(x => console.log(x));
-    }
+    source$.subscribe(x => console.log(x));
+  }
 }
 
 export default Operator;
