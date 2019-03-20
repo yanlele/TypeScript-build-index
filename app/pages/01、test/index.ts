7612dcfe2193eda584b09ea68543f00e005c6e11
yanle <yanle@yanledeMacBook-Pro.local>
2019-03-20 21:58:12 +0800

    [+]实现点击批量下载图片

--- app/pages/01、test/index.ts
+++ app/pages/01、test/index.ts
@@ -4,17 +4,30 @@
  * create time 2018-11-24 22:52
  */
 
-import Main from './Main';
+// import Main from './Main';
 import './index.less';
-import Store from './Store';
-let helloTemplate = require('../../components/template/hello.hbs');
+import $ from 'jquery';
+// import Store from './Store';
+//
+// let helloTemplate = require('../../components/template/hello.hbs');
+//
+// Store.getInstance()._id;
+//
+// let helloTemplateComponent = helloTemplate({
+//   name: 'yanle',
+//   a: 1,
+//   b: 2
+// });
+//
+// document.getElementById('container').innerHTML = helloTemplateComponent;
 
-Store.getInstance()._id;
-
-let helloTemplateComponent = helloTemplate({
-    name: 'yanle',
-    a: 1,
-    b: 2
+document.querySelector('.down-button').addEventListener('click', () => {
+  for (let i = 0; i < 5; i += 1) {
+    const img = document.createElement('a');
+    img.setAttribute('target', '_blank');
+    img.setAttribute('download', '1.jpg');
+    img.setAttribute('href', '/img/1691bb1348a07d78b48cdeba8dc4c9d4.jpg');
+    img.innerText = 'img 下载';
+    img.click();
+  }
 });
-
-document.getElementById('container').innerHTML = helloTemplateComponent;
